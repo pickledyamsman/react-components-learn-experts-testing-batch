8a94135a055598fd31efca4b3d8d887130282346
Matthew <pickledyamsman@gmail.com>
2016-09-20 11:52:44 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 const app = "I don't do much.";
+
+
+const ShoppingList = React.createClass({
+  render() {
+    return React.createElement('ul', {}, [
+      React.createElement('li', {}, 'Bananas'),
+      React.createElement('li', {}, 'Vanilla ice cream'),
+      React.createElement('li', {}, 'Chocolate'),
+    ]);
+  }
+});
+
+class Button extends React.Component {
+  render() {
+    return
+React.createElement('button', {}, 'Click me!');
+  }
+}
+
+const nestedButtons =
+React.createElement('div', {}, [
+  React.createElement(Button),
+  React.createElement(Button),
+]);
